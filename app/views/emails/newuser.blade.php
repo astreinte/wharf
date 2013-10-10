4e50acfac1da6a79af79a4afbe03cfaa385925d4
digithis <rolletremi@gmail.com>
2013-10-10 17:14:44 +0200

    styles for mails

--- app/views/emails/newuser.blade.php
+++ app/views/emails/newuser.blade.php
@@ -11,23 +11,23 @@
                 background-color:url(../img/fd.png);
             }
             a{
-                color:rgb(182, 85, 85);
+                color:rgb(182, 85, 85)!important;
                 font-weight: bold;
                 text-decoration: none;
-                text-shadow: 2px 2px 0px rgba(255, 1, 0, 0.1);
+                text-shadow: 2px 2px 0px rgba(255, 1, 0, 0.1)!important;
             }
             h1{
-                font-size: 22px;
-                font-weight: normal;
-                color:rgb(48, 46, 46);
-                text-shadow: 2px 2px 0px rgba(150, 150, 150, 0.10);
+                font-size: 22px!important;
+                font-weight: normal!important;
+                color:rgb(48, 46, 46)!important;
+                text-shadow: 2px 2px 0px rgba(150, 150, 150, 0.10)!important;
             }
             p{
-                font-size: 14px
+                font-size: 13px!important
             }
             .header{
                 padding-bottom: 10px;
-                border-bottom: 1px dotted rgb(243, 231, 231);
+                 border-bottom: 1px dotted rgb(243, 231, 231)!important;
             }
             .it{
                 font-style: italic;
@@ -35,7 +35,8 @@
     </style>
     <body>
         @include('emails.parts.header')
-        <h2>Confirmation d'inscription sur l'extranet de Wharf</h2>
+        <h1>Confirmation d'inscription sur l'extranet de Wharf</h1>
+        <div style="font-size: 13px">
         <p>
             Bonjour <strong>{{$firstname}} {{$lastname}}</strong>, votre compte extranet a bien été créé.
         </p>
@@ -47,5 +48,6 @@
         <p>Merci pour votre inscription.</p>
         <p>Cordialement,</p>
         <p>L'equipe Wharf.</p>
+        </div>
     </body>
 </html>
--- app/views/emails/reminder.blade.php
+++ app/views/emails/reminder.blade.php
@@ -9,23 +9,23 @@
                 background-color:url(../img/fd.png);
             }
             a{
-                color:rgb(182, 85, 85);
+                color:rgb(182, 85, 85)!important;
                 font-weight: bold;
                 text-decoration: none;
-                text-shadow: 2px 2px 0px rgba(255, 1, 0, 0.1);
+                text-shadow: 2px 2px 0px rgba(255, 1, 0, 0.1)!important;
             }
             h1{
                 font-size: 22px;
                 font-weight: normal;
-                color:rgb(48, 46, 46);
-                text-shadow: 2px 2px 0px rgba(150, 150, 150, 0.10);
+                color:rgb(48, 46, 46)!important;
+                text-shadow: 2px 2px 0px rgba(150, 150, 150, 0.10)!important;
             }
             p{
                 font-size: 14px
             }
             .header{
                 padding-bottom: 10px;
-                border-bottom: 1px dotted rgb(243, 231, 231);
+                border-bottom: 1px dotted rgb(243, 231, 231)!important;
             }
             .it{
                 font-style: italic;
@@ -35,9 +35,11 @@
     <body>
         @include('emails.parts.header')
         <h1>Votre nouveau mot de passe a été créé</h1>
+                <div style="font-size: 13px">
          <p class="it">Votre nouveau mot de passe <strong>{{ $password }}</strong></p>
         <p>
             Connectez vous sur {{ HTML::link('login')}}
         </p>
+                </div>
     </body>
 </html>
